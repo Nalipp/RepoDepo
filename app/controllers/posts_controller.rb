2579d87dcd3216f0c45a079d7add2ca73f9de74c
Nalipp <nateplipp@gmail.com>
2016-01-22 16:24:41 +0900

    build post controller and database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,51 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :show, :destroy]
+
   def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Post was created successfully"
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post was successfully updated"
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @post.destroy
+    flash[:danger] = "Post has been destroyed"
+    redirect_to posts_path
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :description, :link)
+    end
 end
