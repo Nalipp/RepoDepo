9d521ee094a3fbb9666d1a86b5530691c568974f
Nalipp <nateplipp@gmail.com>
2016-01-22 17:34:16 +0900

    add user id to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,15 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def edit
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
+
     if @post.save
       flash[:success] = "Post was created successfully"
       redirect_to post_path(@post)
