df323af997055d6e415af931fa9cf85cbf531082
Nalipp <nateplipp@gmail.com>
2016-01-24 21:52:43 +0900

    update styling and controller redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,9 +29,9 @@ class UsersController < ApplicationController
     current_user.friendships.build(friend_id: @friend.id)
 
     if current_user.save
-      redirect_to my_friends_path, notice: "Friend was successfully added"
+      redirect_to :back, notice: "Friend was successfully added"
     else
-      redirect_to my_friends_path, flash[:error] = "There was an error with adding user as friend"
+      redirect_to :back, flash[:error] = "There was an error with adding user as friend"
     end
   end
 
