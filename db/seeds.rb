34899636337738e4d8c43c27ab5ffc3e2ec15120
Nalipp <nateplipp@gmail.com>
2016-01-29 21:50:35 +0900

    add faker gem seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+User.create!(name:  "Nate Lipp",
+             email: "nateplipp@gmail.com",
+             github: "github.com/Nalipp",
+             password:              "password",
+             password_confirmation: "password",
+             admin: "true")
 
-99.times do |n|
+Post.create!(title: "Add upvotes with acts as votable gem",
+             description: "I was able to add upvote and downvote with the votable
+                        gem but would like to learn more about using it with AJAX",
+             link: "https://github.com/Nalipp/Share-my-code",
+             user_id: "1",
+)
+
+20.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = Faker::Internet.free_email
+  github = "github.com"
   password = "password"
+
+  title = Faker::Hipster.word
+  description = Faker::Hipster.sentence
+  link = "https://github.com"
+  user_id = "#{n+1}"
+
   User.create!(name:  name,
                email: email,
+               github: github,
                password:              password,
                password_confirmation: password)
+
+  Post.create!(title: title,
+               description: description,
+               link: link,
+               user_id: user_id)
 end
