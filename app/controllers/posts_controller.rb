6858ea2652c531d8fc502c7ed0796a472316ed4f
Nalipp <nateplipp@gmail.com>
2016-01-30 14:20:10 +0900

    debug seed.rb erro

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.paginate(page: params[:page], per_page: 12)
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 12)
   end
 
   def show
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def my_friends
-    @friendships = current_user.friends
+    @friendships = current_user.friends.paginate(page: params[:page], per_page: 12)
   end
 
   def search
